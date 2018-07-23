45e859fc890c3c7b642dac66dfcb2b41863c78cd
Adriana Matsufuji <adrianabm@gmail.com>
2018-07-23 16:28:06 +0200

    Create signup mutation

---
+++ server/schema/mutations.js
@@ -0,0 +1,24 @@
+const graphql = require('graphql')
+const { GraphQLObjectType, GraphQLString } = graphql
+const UserType = require('./types/user_type')
+const AuthService = require('../services/auth')
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        // do not put business logic inside mutations,
+        // delegate it to other helpers instead
+        return AuthService.signup({ email, password, req })
+      }
+    }
+  }
+})
+
+module.exports = mutation
