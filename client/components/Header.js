56c18b0f5dd3bb592a9ce97e7d63bd2317071651
Adriana Matsufuji <adrianabm@gmail.com>
2018-07-24 15:17:38 +0200

    Adjust header buttons to work correctly

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
 
 import CurrentUser from '../queries/CurrentUser'
+import Logout from '../mutations/Logout'
 
 class Header extends Component {
+  onLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query: CurrentUser }]
+    })
+  }
+
+  renderButtons() {
+    const { loading, currentUser } = this.props.data
+
+    if (loading) {
+      return <div />
+    }
+
+    if (currentUser) {
+      return (
+        <li>
+          <a onClick={this.onLogoutClick.bind(this)}>Logout</a>
+        </li>
+      )
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      )
+    }
+  }
+
   render() {
-    console.log(this.props)
-    return <div>Header</div>
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
+      </nav>
+    )
   }
 }
 
-export default graphql(CurrentUser)(Header)
+export default graphql(Logout)(graphql(CurrentUser)(Header))
